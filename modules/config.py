07fa567ab72e32e71b62777c03f37275af8303f6
QuantiKa14 <Quantika14@users.noreply.github.com>
2019-09-30 13:52:54 +0200

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,18 @@
+#Printables color codes
+class colores:
+    header = '\033[95m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    alert = '\033[93m'
+    fail = '\033[91m'
+    normal = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+#If you want to change default Emails list
+dir_listEmails = "emails.txt"
+
+#Results counter
+count_netflix = 0
+count_spotify = 0
+count_leaked = 0
