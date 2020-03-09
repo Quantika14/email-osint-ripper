99b46040253c79abc2a1a3ec6eaa8f6ab8a6f6cd
QuantiKa14 <Quantika14@users.noreply.github.com>
2020-03-09 13:39:25 +0100

    add check_facebook
    Facebook y quitado bug del modulo "config" al hacer print en varias líneas.

--- eo-ripper.py
+++ eo-ripper.py
@@ -47,7 +47,7 @@ def check_wordpress(email):
 		if "Invalid" in div:
 			print("|--[INFO][WordPress][CHECK][>] Account doesn't exist...")
 	except:
-		print(C.C.colores.alert + "|--[WARNING][LinkedIn][>] Error..." + C.C.colores.normal)
+		print(C.colores.alert + "|--[WARNING][LinkedIn][>] Error..." + C.colores.normal)
 
 def check_pastebin(email):
 	url = "http://pastebin.com/search?q=" + email.replace(" ", "+")
@@ -66,9 +66,9 @@ def check_AccountTwitter(email):
 		for text in soup.findAll("h1"):
 			text = R.remove_tags(str(text))
 			if "Sorry" in text or "Lo sentimos," in text:
-				print("|--[INFO][Twitter][" + C.C.colores.blue+ username + C.C.colores.normal + "][>] Account doesn't exist...")
+				print("|--[INFO][Twitter][" + C.colores.blue+ username + C.colores.normal + "][>] Account doesn't exist...")
 			else:
-				print(C.C.colores.green + "|--[INFO][Twitter][" + C.colores.blue+ username + C.colores.green + "][>] The account exist." + C.colores.normal)
+				print(C.colores.green + "|--[INFO][Twitter][" + C.colores.blue+ username + C.colores.green + "][>] The account exist." + C.colores.normal)
 	except urllib.error.HTTPError:
 		print(C.colores.alert + "|--[404 HTTP RESPONSE][Check_AccountTwitter][>] 404 HTTP Twitter error..." + C.colores.normal)
 
@@ -99,38 +99,59 @@ def check_haveibeenpwned(email):
 	else:
 		print("|--[INFO][HAVEIBEENPWNED][>] Your email doesn't appear in leaks...")
 
+def check_facebook(email):
+
+	r = br.open('https://mbasic.facebook.com/')
+	br.select_form(nr=0)
+	br.form["email"] = email
+	br.form["pass"] = "123456"
+	br.submit()
+	respuestaURL = br.response().geturl()
+
+	html =  br.response().read()
+	soup = BeautifulSoup(html, "html.parser")
+	div = soup.find("div",{"id":"login_error"})
+	if "ninguna" in R.remove_tags(str(div)):
+		print("|--[INFO][FACEBOOK][CHECK][>] Account doesn't exist...")
+	else:
+		print("|--[INFO][FACEBOOK][CHECK][>] The account exist...")
+
+
 def check_emailrep(email):
 	url = "https://emailrep.io/" + email
 	JSON = json.loads(requests.get(url).text)
+	try:
+		print("|--[INFO][REPUTATION][>] " + JSON["reputation"])
+		print("|--[INFO][SUSPICIUS][>] " + str(JSON["suspicious"]))
+		print("|--[INFO][BLACK LIST][>] " + str(JSON["details"]["blacklisted"]))
+		print("|--[INFO][MALICIUS ACTIVITY][>] " + str(JSON["details"]["malicious_activity"]))
+		print("|--[INFO][SPAM][>] " + str(JSON["details"]["spam"]))
+		print("|--[INFO][MALICIUS ACTIVITY][>] " + str(JSON["details"]["malicious_activity"]))
+		print("|--[INFO][SPOOFABLE][>] " + str(JSON["details"]["spoofable"]))
+		print("|--[INFO][SPF STRICT][>] " + str(JSON["details"]["spf_strict"]))
+		print("|--[INFO][DMARC ENFORCED][>] " + str(JSON["details"]["dmarc_enforced"]))
+
+		DOMAIN = email.split("@")
+		print("|--[INFO][DOMAIN][>] Analyzing the domain " + DOMAIN[1])
+		print("|----[INFO][CHECK DOMAIN][>] " + str(JSON["details"]["domain_exists"]))
+		print("|----[INFO][DOMAIN REPUTATION][>] " + str(JSON["details"]["domain_reputation"]))
+		print("|----[INFO][NEW DOMAIN][>] " + str(JSON["details"]["new_domain"]))
+		print("|------[INFO][DAYS SINCE DOMAIN CREATION][>] " + str(JSON["details"]["days_since_domain_creation"]))
+		print("|------[INFO][FREE PROVIDER][>] " + str(JSON["details"]["free_provider"]))
+
+		#RRSS Analyzer
+		print("|--[INFO][PROFILES IN SOCIAL NETWORKS][>] Analyzing...")
+		for profile in JSON["details"]["profiles"]:
+
+			print("|------[INFO][SOCIAL NETWORK][>] " + profile)
+
+		if JSON["details"]["credentials_leaked"]:
+			C.count_leaked += 1
+		if "Spotify" in JSON["details"]["profiles"]:
+			C.count_spotify += 1
 	
-	print("|--[INFO][REPUTATION][>] " + JSON["reputation"])
-	print("|--[INFO][SUSPICIUS][>] " + str(JSON["suspicious"]))
-	print("|--[INFO][BLACK LIST][>] " + str(JSON["details"]["blacklisted"]))
-	print("|--[INFO][MALICIUS ACTIVITY][>] " + str(JSON["details"]["malicious_activity"]))
-	print("|--[INFO][SPAM][>] " + str(JSON["details"]["spam"]))
-	print("|--[INFO][MALICIUS ACTIVITY][>] " + str(JSON["details"]["malicious_activity"]))
-	print("|--[INFO][SPOOFABLE][>] " + str(JSON["details"]["spoofable"]))
-	print("|--[INFO][SPF STRICT][>] " + str(JSON["details"]["spf_strict"]))
-	print("|--[INFO][DMARC ENFORCED][>] " + str(JSON["details"]["dmarc_enforced"]))
-
-	DOMAIN = email.split("@")
-	print("|--[INFO][DOMAIN][>] Analyzing the domain " + DOMAIN[1])
-	print("|----[INFO][CHECK DOMAIN][>] " + str(JSON["details"]["domain_exists"]))
-	print("|----[INFO][DOMAIN REPUTATION][>] " + str(JSON["details"]["domain_reputation"]))
-	print("|----[INFO][NEW DOMAIN][>] " + str(JSON["details"]["new_domain"]))
-	print("|------[INFO][DAYS SINCE DOMAIN CREATION][>] " + str(JSON["details"]["days_since_domain_creation"]))
-	print("|------[INFO][FREE PROVIDER][>] " + str(JSON["details"]["free_provider"]))
-
-	#RRSS Analyzer
-	print("|--[INFO][PROFILES IN SOCIAL NETWORKS][>] Analyzing...")
-	for profile in JSON["details"]["profiles"]:
-
-		print("|------[INFO][SOCIAL NETWORK][>] " + profile)
-
-	if JSON["details"]["credentials_leaked"]:
-		C.count_leaked += 1
-	if "Spotify" in JSON["details"]["profiles"]:
-		C.count_spotify += 1
+	except:
+		print("Exceeded daily limit. You can continue using the application with Dante's Gates Mobile here: https://play.google.com/store/apps/details?id=com.quantika14.dantes_gates_mobile&gl=ES")
 
 
 # Email spoofing generator php
@@ -174,6 +195,7 @@ Date latest version: 31/07/2018 | Version: 1.2.1
 Date latest version: 30/01/2019 | Version: 1.2.3
 Date latest version: 30/08/2019 | Version: 1.3.0
 Date latest version: 30/09/2019 | Version: 1.4.1
+Date latest version: 09/03/2020 | Version: 1.5.1
 -------------------------------------------------------------------------------------
 """)
 
@@ -198,32 +220,20 @@ def attack(email):
 	if R.check_email(email):
 		print("[INFO][TARGET][>] Hello, " + email + " is correct.")
 		
-		try:
-			is_valid = validate_email(email,verify=True)
-			if is_valid:
-				print("[INFO][TARGET][>] " + email)
-				print("|--[INFO][EMAIL][>] Email validated...")
+		print("[INFO][TARGET][>] " + email)
+		print("|--[INFO][EMAIL][>] Email validated...")
 
-				#CALL THE ACTION
-				check_emailrep(email)
-				check_AccountTwitter(email)
-				check_wordpress(email)
-				check_netflix(email)
-				check_haveibeenpwned(email)
-				check_pastebin(email)
-			else:
-				print("[INFO][TARGET][>] " + email)
-				print("|--[INFO][EMAIL][>] It's not created...")
-
-		except:
-			print("[INFO][TARGET][>] " + email)
-			print(C.colores.alert + "|--[INFO][EMAIL][>] No verification possible... " + C.colores.normal)
-			print("|--[INFO][>] have you installed all the libraries? pip3 install -r requeriments.txt")
+		#CALL THE ACTION
+		check_emailrep(email)
+		check_AccountTwitter(email)
+		check_wordpress(email)
+		check_netflix(email)
+		check_haveibeenpwned(email)
+		check_pastebin(email)
+		check_facebook(email)
 
 	else:
 		print("[INFO][TARGET][>] Hello, " + email + " doesn't a email. Try again!'")
-
-
 
 def main():
 	global emails_list
